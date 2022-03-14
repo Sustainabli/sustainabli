363902936ab62166126688601f2dbce999a32b8b
Michael Li <mli25782@umd.edu>
2022-03-14 19:34:57 -0400

    day/night data filter

--- src/components/SashChart/SashChart.js
+++ src/components/SashChart/SashChart.js
@@ -9,6 +9,10 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
+
+
 import data from '../../mock-data/mock-sash.json';
 import { CHART_COLORS } from '../../utils/Constants.js';
 
@@ -22,8 +26,25 @@ ChartJS.register(
 );
 
 class SashChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDayData: false,
+    };
+  }
+
+  filterDayNightData = (datum, showDay) => {
+    const date = new Date(datum['time']);
+    const dateHour = date.getHours();
+    if (showDay) {
+      return dateHour > 6 && dateHour < 18;
+    }
+    return dateHour <= 6 || dateHour >= 18;
+  }
+
   render() {
-    const labels = data.map(datum => new Date(datum.time).toLocaleString());
+    const {showDayData} = this.state;
+    const labels = data.filter(datum => this.filterDayNightData(datum, showDayData)).map(datum => new Date(datum.time).toLocaleString());
 
     // For now we are extracting keys from first entry. Likely first data entry might NOT have all keys so we need a better solution down the line
     // We also want to remove 'time' from the list of data keys (this simplifies looping through line graph keys)
@@ -35,7 +56,8 @@ class SashChart extends React.Component {
         const colorIndex = dataKeys.indexOf(key);
         return {
           label: key,
-          data: data.map(datum => datum[key]),
+          // data: data.map(datum => datum[key]),
+          data: data.filter(datum => this.filterDayNightData(datum, showDayData)).map(datum => datum[key]),
           borderColor:  CHART_COLORS[colorIndex],
           backgroundColor: `${CHART_COLORS[colorIndex]}80`,
         };
@@ -56,6 +78,13 @@ class SashChart extends React.Component {
     };
     return (
       <div className="Sash-Chart">
+        <Toggle
+          id='show-day-data'
+          defaultChecked={showDayData}
+          onChange={() => this.setState({showDayData: !showDayData})}
+        />
+        <br/>
+        <label htmlFor='show-day-data'>Show Day Data</label>
         <Line options={options} data={sashData}/>
       </div>
     );
