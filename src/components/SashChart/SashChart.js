0227db4ada082d40477c554d926a8a9f7e31ff17
Michael Li <mli25782@umd.edu>
2022-03-22 00:00:54 -0400

    added time range filtering and reformatted code

--- src/components/SashChart/SashChart.js
+++ src/components/SashChart/SashChart.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Modal from 'react-bootstrap/Modal';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,9 +12,17 @@ import {
   Title,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import {Line} from 'react-chartjs-2';
+import Toggle from 'react-toggle';
+import "react-toggle/style.css";
 
 
+import {
+  NONE,
+  TIME_GRANULARITIES,
+  TIME_RANGES,
+} from '../../utils/Constants.js';
+import {fetchFilteredData} from '../../utils/Utils.js';
 import {CHART_COLORS} from '../../utils/Constants.js';
 
 ChartJS.register(
@@ -23,13 +35,77 @@ ChartJS.register(
 );
 
 class SashChart extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFilterModal: false,
+      filters: {
+        granularity: NONE,
+        showDayData: false,
+        timeRange: "",
+      },
+      // These are the filters the user has SELECTED but hasn't SUBMITTED on the filter modal
+      tempFilters: {
+        granularity: NONE,
+        showDayData: false,
+        timeRange: "",
+      },
+      filteredData: [],
+    };
+  }
+
+  async componentDidMount() {
+    const {filters} = this.state;
+    this.setState({filteredData: await fetchFilteredData(filters)})
+  }
+
+  onShowFilterModal = () => {
+    this.setState({showFilterModal: true})
+  }
+
+  onHideFilterModal = () => {
+    const {filters} = this.state;
+    this.setState({tempFilters: {...filters}, showFilterModal: false})
+  }
+
+  // This fxn only updates tempFilters
+  onChangeTimeRange = range => {
+    const {tempFilters} = this.state;
+    tempFilters.timeRange = range;
+    this.setState({tempFilters: {...tempFilters}});
+  }
+
+  // This fxn only updates tempFilters
+  onChangeTempGranularity = granularity => {
+    const {tempFilters} = this.state;
+    tempFilters.granularity = granularity;
+    this.setState({tempFilters: {...tempFilters}});
+  }
+
+  // This fxn only updates tempFilters
+  onChangeTempShowDayData = () => {
+    const {tempFilters} = this.state;
+    tempFilters.showDayData = !tempFilters.showDayData;
+    this.setState({tempFilters: {...tempFilters}});
+  }
+
+  // This fxn will update the actual filters
+  onSubmitUpdateFilters = async () => {
+    const {tempFilters} = this.state;
+    this.setState({
+      showFilterModal: false,
+      filters: {...tempFilters},
+      filteredData: await fetchFilteredData(tempFilters)});
+  }
+
   render() {
-    const {filteredData} = this.props;
+    const {showFilterModal, tempFilters, filteredData} = this.state;
     const labels = filteredData.map(datum => new Date(datum.time).toLocaleString());
 
-    // For now we are extracting keys from first entry. Likely first data entry might NOT have all keys so we need a better solution down the line
+    // First check if the data has been loaded yet. If it hasn't either filteredData will be null or filteredData.length will be 0
     // We also want to remove 'time' from the list of data keys (this simplifies looping through line graph keys)
-    const dataKeys = Object.keys(filteredData[0]).filter(key => key !== 'time');
+    const dataKeys = filteredData && filteredData.length ? Object.keys(filteredData[0]).filter(key => key !== 'time') : [];
 
     const sashData = {
       labels,
@@ -56,8 +132,71 @@ class SashChart extends React.Component {
         },
       },
     };
+
     return (
       <div className="Sash-Chart">
+        <Modal
+          show={showFilterModal}
+          onHide={this.onHideFilterModal}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Filter Data</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <span> Look at Past
+              <ButtonGroup className="mb-2" aria-label="Average Over">
+                {TIME_RANGES.map((range, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`time-range-${idx}`}
+                    type="radio"
+                    variant="primary"
+                    name="radio"
+                    value={range}
+                    checked={tempFilters.timeRange === range}
+                    onChange={e => this.onChangeTimeRange(e.currentTarget.value)}
+                  >
+                    {range}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </span>
+            <br/>
+            <span> Average Over
+              <ButtonGroup className="mb-2" aria-label="Average Over">
+                {TIME_GRANULARITIES.map((granularity, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`time-granularity-${idx}`}
+                    type="radio"
+                    variant="primary"
+                    name="radio"
+                    value={granularity}
+                    checked={tempFilters.granularity === granularity}
+                    onChange={e => this.onChangeTempGranularity(e.currentTarget.value)}
+                  >
+                    {granularity}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </span>
+            <br/>
+            <Toggle
+              id='show-day-data'
+              defaultChecked={tempFilters.showDayData}
+              onChange={this.onChangeTempShowDayData}
+            />
+            <br/>
+            <label htmlFor='show-day-data'>Show Day Data</label>
+            <br/>
+            <br/>
+            <Button variant="primary" onClick={this.onSubmitUpdateFilters}>Update Filters</Button>
+          </Modal.Body>
+        </Modal>
+        <Button variant="primary" onClick={this.onShowFilterModal}>Filter Data</Button>
         <Line options={options} data={sashData}/>
       </div>
     );
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -3,6 +3,17 @@
 // Fetches filtered data from database based on filters and data category
 // TODO: For now data category will default to null since we only have sash data
 export const fetchFilteredData = async (filters, category?=null) => {
-  return fetch(`/${filters.granularity}`)
+  // Parse out which fields we want from filters
+  const {granularity, timeRange} = filters;
+  // We need to add offset for now since data is from 2018
+  const useOffset = true;
+  let fetchURL = `/${granularity}`;
+  if (timeRange) {
+    fetchURL += `?relative=${timeRange}`
+  }
+  if (timeRange && useOffset) {
+    fetchURL += `&offset=2018-10-11`
+  }
+  return fetch(fetchURL)
     .then(res => res.json())
 }
