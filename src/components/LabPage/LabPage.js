4b038d7aeab475bfa4e7c6a3ddc0acd75affc7c7
Andrew Hong <ahong8595@gmail.com>
2022-12-27 11:19:54 -0500

    Refactor chartTemplate

--- src/components/LabPage/LabPage.js
+++ src/components/LabPage/LabPage.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col';
 import ReactLoading from 'react-loading';
 import LineGraph from '../LineGraph/LineGraph.js';
 import FilterOptions from '../FilterOptions/FilterOptions';
+import ChartTemplate from '../ChartTemplate/ChartTemplate.js';
+
 import {
   DATA_FORMATS,
   DATA_TYPES,
@@ -27,6 +29,7 @@ class LabPage extends React.Component {
     super();
     this.state = {
       lineGraphData: {},
+      barGraphData: {},
       loading: true,
     };
   }
@@ -51,20 +54,37 @@ class LabPage extends React.Component {
       startDate
     );
 
+    const responseBar = await fetchFilteredData(
+      DATA_TYPES.cfm,
+      DATA_FORMATS.singleLab,
+      GRAPH_TYPES.line,
+      TIME_GRANULARITIES.week,
+      TIME_OF_DAY.all,
+      labFumehoodMapping,
+      startDate
+    );
+
     this.setState({
       lineGraphData: response,
+      barGraphData: responseBar,
       loading: false,
     });
   }
 
   setFilteredLineGraphData = (data) => {
+    console.log("changing", data)
     this.setState({ lineGraphData: data });
+  };
+
+  setFilteredBarGraphData = (data) => {
+    this.setState({ barGraphData: data });
   };
 
   render() {
     const { lab } = this.props;
-    const { lineGraphData, loading } = this.state;
+    const { lineGraphData, barGraphData, loading } = this.state;
 
+    const barChartTitle = `Fumehood CFM Data for ${capitalizeString(lab)} Lab (Bar)`;
     const chartTitle = `Fumehood CFM Data for ${capitalizeString(lab)} Lab`;
 
     return (
@@ -77,15 +97,34 @@ class LabPage extends React.Component {
               <Col md={2}>
                 <FilterOptions
                   lab={lab}
+                  setFilteredLineGraphData={this.setFilteredBarGraphData}
+                />
+              </Col>
+              <Col md={10}>
+                {Object.keys(barGraphData).length > 0 && (
+                  <ChartTemplate
+                    lab={lab}
+                    filteredData={barGraphData}
+                    chartTitle={barChartTitle}
+                    chartType={"bar"}
+                  />
+                )}
+              </Col>
+            </Row>
+            <Row>
+              <Col md={2}>
+                <FilterOptions
+                  lab={lab}
                   setFilteredLineGraphData={this.setFilteredLineGraphData}
                 />
               </Col>
               <Col md={10}>
                 {Object.keys(lineGraphData).length > 0 && (
-                  <LineGraph
+                  <ChartTemplate
                     lab={lab}
                     filteredData={lineGraphData}
                     chartTitle={chartTitle}
+                    chartType={"line"}
                   />
                 )}
               </Col>
