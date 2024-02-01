e3f00ad34f57288612d4a7507b82cead532cfd96
Jason Zhong <jasonzhong0408@gmail.com>
2024-01-31 23:02:56 -0500

    prettier + scss file

--- src/components/FumeHoodsPage/FumeHoodsPage.js
+++ src/components/FumeHoodsPage/FumeHoodsPage.js
@@ -1,60 +1,58 @@
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { withAuth0 } from '@auth0/auth0-react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
-import Header from '../../utils/components/Header/Header';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import FumeModalForm from './components/FumeModalForm';
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { withAuth0 } from "@auth0/auth0-react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+import Header from "../../utils/components/Header/Header";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import FumeModalForm from "./components/FumeModalForm";
+import "./FumeHoodsPage.scss";
 import {
   // Form types
   UPDATE_FUME_HOOD_INFO,
-	CREATE_SENSOR,
+  CREATE_SENSOR,
   // Recoil states
   USER_INFO_STATE,
   METRIC_TYPE_AIRFLOW,
-} from '../../utils/Constants';
+} from "../../utils/Constants";
 import {
-	fetchAllSensorForOrganization,
-	convertSashHeightToMetricValue
-  } from '../../utils/Utils';
+  fetchAllSensorForOrganization,
+  convertSashHeightToMetricValue,
+} from "../../utils/Utils";
 
 function FumeHoodsPage(props) {
-
   const editFumeModal = (sensorInfo) => {
-		setShouldShowModalForm(true)
-		setSensor(sensorInfo)
-		setForm(UPDATE_FUME_HOOD_INFO);
-  }
+    setShouldShowModalForm(true);
+    setSensor(sensorInfo);
+    setForm(UPDATE_FUME_HOOD_INFO);
+  };
 
   const createFumeModal = () => {
-		setShouldShowModalForm(true)
-		setSensor(null)
-		setForm(CREATE_SENSOR);
-  }
+    setShouldShowModalForm(true);
+    setSensor(null);
+    setForm(CREATE_SENSOR);
+  };
 
   const closeModal = () => {
-		setShouldShowModalForm(false)
-		setSensor(null)
-  }
+    setShouldShowModalForm(false);
+    setSensor(null);
+  };
 
   function findCFM(sensorName, arr) {
-		let res = arr.find((ele) => ele.fumeHood === sensorName)
-		if (res !== undefined) {
-			res = <ProgressBar variant='info' now={res.normalizedCfm}/>
-		} 
-		else {
-			res = "HEIGHT"
-		}
-		return res;
+    const res = arr.find((ele) => ele.fumeHood === sensorName);
+    return res ? (
+      <ProgressBar variant="info" now={res.normalizedCfm} />
+    ) : (
+      "No Data to Show"
+    );
   }
 
   // availableSensors is a list of the possible sensors
-	// ex: [{fume_hood_name: 'testMac', organization_code: 'TTT', sensor_id: 'testMac'}, ...]
-	const availableSensors = props.availableSensors;
+  // ex: [{fume_hood_name: 'testMac', organization_code: 'TTT', sensor_id: 'testMac'}, ...]
+  const availableSensors = props.availableSensors;
 
   const [userInfo, _] = useRecoilState(USER_INFO_STATE);
   const [shouldShowModalForm, setShouldShowModalForm] = useState(false);
@@ -63,96 +61,106 @@ function FumeHoodsPage(props) {
 
   // allSensorsData is a list of js objects which contains the Cfm? at a specific time
   // ex: [{data : {testMac: 5.625, testMax2: 65}, time: "2023"}, ...]
-  const [allSensorsData, setAllSensorsData] = useState([])
-  
+  const [allSensorsData, setAllSensorsData] = useState([]);
+
   useEffect(() => {
-		async function getSensorData() {
-			const res = await fetchAllSensorForOrganization(userInfo.organization_code);
-			setAllSensorsData(res);
-		}
-		getSensorData();
-  }, [])
+    async function getSensorData() {
+      const res = await fetchAllSensorForOrganization(
+        userInfo.organization_code
+      );
+      setAllSensorsData(res);
+    }
+    getSensorData();
+  }, []);
 
   // summedCfmData is an object with the sensors and the summed cfm
   // ex: {testMac: 835, testMax2: 2090}
   const summedCfmData = {};
-  allSensorsData.forEach(datum => Object.entries(datum.data).forEach(([key, value]) => {
-		const cfmValue = convertSashHeightToMetricValue(METRIC_TYPE_AIRFLOW, value);
-		summedCfmData[key] = key in summedCfmData ? summedCfmData[key] + cfmValue : cfmValue;
-  }));
+  allSensorsData.forEach((datum) =>
+    Object.entries(datum.data).forEach(([key, value]) => {
+      const cfmValue = convertSashHeightToMetricValue(
+        METRIC_TYPE_AIRFLOW,
+        value
+      );
+      summedCfmData[key] =
+        key in summedCfmData ? summedCfmData[key] + cfmValue : cfmValue;
+    })
+  );
 
   // Take the largest cfm value, calculate the ratio for the progress bar, and sort summedCfmData in descending order based on this ratio
 
   // orderSummedCfmData is a list of the sensors with the summed and normalized cfm
   // ex: [{fume_hood_name: testMac, cfm : 835, normalizedCfm: 40}, ...]
   const ratio = Math.max.apply(Math, Object.values(summedCfmData)) / 100;
-  const orderedSummedCfmData = Object.entries(summedCfmData).map(([key, value]) => ({
-		fumeHood: key,
-		cfm: value,
-		normalizedCfm: Math.round(value / ratio)
-  }));
+  const orderedSummedCfmData = Object.entries(summedCfmData).map(
+    ([key, value]) => ({
+      fumeHood: key,
+      cfm: value,
+      normalizedCfm: Math.round(value / ratio),
+    })
+  );
   orderedSummedCfmData.sort((a, b) => b.normalizedCfm - a.normalizedCfm);
 
   return (
-    <Container className='FumeHoodsPage' fluid>
-		{
-			shouldShowModalForm && 
-			<FumeModalForm
-				formType={form}
-				selectedSensorInfo={sensor}
-				clearModalFormType={closeModal}
-			/>
-		}
-      <Header pageName='Fume Hoods Page' />
+    <Container className="FumeHoodsPage" fluid>
+      {shouldShowModalForm && (
+        <FumeModalForm
+          formType={form}
+          selectedSensorInfo={sensor}
+          clearModalFormType={closeModal}
+        />
+      )}
+      <Header pageName="Fume Hoods Page" />
       <Row>
-				<Col md={3}>
-					<h3>Fume Hoods </h3>
-				</Col>
-				<Col/>
-				<Col md="auto">
-					<Button onClick={() => createFumeModal(sensor)} variant='info'>
-						Create New Fume Hood
-					</Button>
-				</Col>
+        <Col md={3}>
+          <h3>Fume Hoods </h3>
+        </Col>
+        <Col />
+        <Col md="auto">
+          <Button onClick={() => createFumeModal(sensor)} variant="info">
+            Create New Fume Hood
+          </Button>
+        </Col>
       </Row>
 
       <Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>Fume Hood</th>
-						<th>Org</th>
-						<th>Building</th>
-						<th>Room</th>
-						<th>Lab</th>
-						<th>Sensor MAC</th>
-						<th>Real-Time Sash Height</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{availableSensors.map((sensor, index) => (
-						<tr key={index}>
-							<td>{sensor.fume_hood_name}</td>
-							<td>{userInfo.organization_code}</td>
-							<td>BUILDING</td>
-							<td>ROOM</td>
-							<td>LAB</td>
-							<td>{sensor.sensor_id}</td>
-							{/* Unable to set progress bar to 25% of table width through scss so doing inline styling instead */} 
-							<td style={{width: '25%'}}>
-							{findCFM(sensor.fume_hood_name, orderedSummedCfmData)}
-							</td>
-							<td>
-								<Button 
-									className='showModalFormButton' 
-										variant='dark'
-									onClick={() => editFumeModal(sensor)}>
-										Edit
-								</Button>
-							</td>
-	    			</tr>
-	  			))}
-				</tbody>
+        <thead>
+          <tr>
+            <th>Fume Hood</th>
+            <th>Org</th>
+            <th>Building</th>
+            <th>Room</th>
+            <th>Lab</th>
+            <th>Sensor MAC</th>
+            <th>Real-Time Sash Height</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {availableSensors.map((sensor, index) => (
+            <tr key={index}>
+              <td>{sensor.fume_hood_name}</td>
+              <td>{userInfo.organization_code}</td>
+              <td>BUILDING</td>
+              <td>ROOM</td>
+              <td>LAB</td>
+              <td>{sensor.sensor_id}</td>
+              {/* Unable to set progress bar to 25% of table width through scss so doing inline styling instead */}
+              <td style={{ width: "25%" }}>
+                {findCFM(sensor.fume_hood_name, orderedSummedCfmData)}
+              </td>
+              <td>
+                <Button
+                  className="showModalFormButton"
+                  variant="dark"
+                  onClick={() => editFumeModal(sensor)}
+                >
+                  Edit
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </Container>
   );
--- src/components/FumeHoodsPage/components/FumeModalForm.js
+++ src/components/FumeHoodsPage/components/FumeModalForm.js
@@ -1,170 +1,136 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import { CREATE_SENSOR, UPDATE_FUME_HOOD_INFO } from '../../../utils/Constants';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import "../FumeHoodsPage.scss";
+import { CREATE_SENSOR, UPDATE_FUME_HOOD_INFO } from "../../../utils/Constants";
 
 class FumeModalForm extends React.Component {
   onCloseModal = () => {
     const { clearModalFormType } = this.props;
     clearModalFormType();
-  }
+  };
 
   renderFormContent = () => {
-    const {
-      formType,
-      selectedSensorInfo,
-    } = this.props;
+    const { formType, selectedSensorInfo } = this.props;
 
-    let formTitle = '';
+    let formTitle = "";
     let modalBody;
 
     switch (formType) {
       case UPDATE_FUME_HOOD_INFO:
-        formTitle = 'Update Fume Hood Page';
-        modalBody = 
+        formTitle = "Update Fume Hood Page";
+        modalBody = (
           <Form>
             <Modal.Body>
-              <Form.Group controlId='fumeHoodNameUpdatePage'>
+              <Form.Group controlId="fumeHoodNameUpdatePage">
                 <Form.Label>
                   <h5>Fume Hood Name</h5>
                 </Form.Label>
                 <Form.Control
-                  type='input'
-                  placeholder='Fume Hood Name'
-                  defaultValue={selectedSensorInfo.fume_hood_name}
+                  type="input"
+                  placeholder={selectedSensorInfo.fume_hood_name}
                   required
                 />
                 <Form.Label>
                   <h5>Organization</h5>
                 </Form.Label>
                 <Form.Control
-                  type='input'
-                  placeholder='Organization'
-                  defaultValue='Organization'
+                  type="input"
+                  placeholder="Organization"
                   required
                 />
                 <Form.Label>
                   <h5>Building</h5>
                 </Form.Label>
-                <Form.Control
-                  type='input'
-                  placeholder='Building'
-                  defaultValue='Building'
-                  required
-                />
+                <Form.Control type="input" placeholder="Building" required />
                 <Form.Label>
                   <h5>Room</h5>
                 </Form.Label>
-                <Form.Control
-                  type='input'
-                  placeholder='Room'
-                  defaultValue='Room'
-                  required
-                />
+                <Form.Control type="input" placeholder="Room" required />
                 <Form.Label>
                   <h5>Lab</h5>
                 </Form.Label>
-                <Form.Control
-                  type='input'
-                  placeholder='Lab'
-                  defaultValue='Lab'
-                  required
-                />
+                <Form.Control type="input" placeholder="Lab" required />
               </Form.Group>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant='dark' type='submit'> Update Fume Hood </Button>
+              <Button variant="info" type="submit">
+                {" "}
+                Update Fume Hood{" "}
+              </Button>
             </Modal.Footer>
-          </Form> 
+          </Form>
+        );
         break;
 
       case CREATE_SENSOR:
-        formTitle = 'Create Fume Hood';
-          modalBody = 
-            <Form>
-              <Modal.Body>
-                <Form.Group controlId='fumeHoodNameCreatePage'>
-                  <Form.Label>
-                    <h5>Fume Hood Name</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Fume Hood Name'
-                    defaultValue='Fume Hood Name'
-                    required
-                  />
-                  <Form.Label>
-                    <h5>Organization</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Organization'
-                    defaultValue='Organization'
-                    required
-                  />
-                  <Form.Label>
-                    <h5>Building</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Building'
-                    defaultValue='Building'
-                    required
-                  />
-                  <Form.Label>
-                    <h5>Room</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Room'
-                    defaultValue='Room'
-                    required
-                  />
-                  <Form.Label>
-                    <h5>Lab</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Lab'
-                    defaultValue='Lab'
-                    required
-                  />
-                  <Form.Label>
-                    <h5>Sensor MAC</h5>
-                  </Form.Label>
-                  <Form.Control
-                    type='input'
-                    placeholder='Sensor MAC'
-                    defaultValue='Sensor MAC'
-                    required
-                  />
-                </Form.Group>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant='dark' type='submit'> Create Fume Hood </Button>
-              </Modal.Footer>
-            </Form> 
-          break;
+        formTitle = "Create Fume Hood";
+        modalBody = (
+          <Form>
+            <Modal.Body>
+              <Form.Group controlId="fumeHoodNameCreatePage">
+                <Form.Label>
+                  <h5>Fume Hood Name</h5>
+                </Form.Label>
+                <Form.Control
+                  type="input"
+                  placeholder="Fume Hood Name"
+                  required
+                />
+                <Form.Label>
+                  <h5>Organization</h5>
+                </Form.Label>
+                <Form.Control
+                  type="input"
+                  placeholder="Organization"
+                  required
+                />
+                <Form.Label>
+                  <h5>Building</h5>
+                </Form.Label>
+                <Form.Control type="input" placeholder="Building" required />
+                <Form.Label>
+                  <h5>Room</h5>
+                </Form.Label>
+                <Form.Control type="input" placeholder="Room" required />
+                <Form.Label>
+                  <h5>Lab</h5>
+                </Form.Label>
+                <Form.Control type="input" placeholder="Lab" required />
+                <Form.Label>
+                  <h5>Sensor MAC</h5>
+                </Form.Label>
+                <Form.Control type="input" placeholder="Sensor MAC" required />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="info" type="submit">
+                {" "}
+                Create Fume Hood{" "}
+              </Button>
+            </Modal.Footer>
+          </Form>
+        );
+        break;
 
-        default:
+      default:
     }
     return {
       formTitle,
-      modalBody
+      modalBody,
     };
-    
-  }
+  };
   render() {
     const { formType } = this.props;
     const { formTitle, modalBody } = this.renderFormContent();
     return (
       <Modal
-        size='lg'
+        size="lg"
         show={formType}
         onHide={() => this.onCloseModal()}
         keyboard={false}
-        backdrop={'static'}
+        backdrop={"static"}
       >
         <Modal.Header closeButton>
           <Modal.Title> {formTitle} </Modal.Title>
@@ -175,4 +141,4 @@ class FumeModalForm extends React.Component {
   }
 }
 
-export default FumeModalForm
+export default FumeModalForm;
